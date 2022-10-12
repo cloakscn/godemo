f92de027c783be541cf46bd9ce132736b87b2e49
王艺 <wy0329@foxmail.com>
2022-10-12 23:58:21 +0800

    grpc.WithPerRPCCredentials

--- demo/demo_12/client/main.go
+++ demo/demo_12/client/main.go
@@ -3,31 +3,46 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
+	// "time"
 
 	v1 "example.com/test/demo/demo_11/proto/gen/interceptor/v1"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"
+	// "google.golang.org/grpc/credentials"
+	// "google.golang.org/grpc/metadata"
 )
+
+type customCredential struct {}
+
+func (c customCredential) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string {
+		"APPID": "2018061036",
+		"APPKEY": "Ycu061036",
+	}, nil
+}
+
+func (c customCredential) RequireTransportSecurity() bool {
+	return false
+}
 
 
 func main() {
-	interceptor := func (ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-		start := time.Now()
-
-		md := metadata.New(map[string]string{
-			"APPID": "2018061036",
-			"APPKEY": "Ycu@061036",
-		})
-		ctx = metadata.NewOutgoingContext(context.Background(), md)
-		err := invoker(ctx, method, req, reply, cc, opts...)
-		fmt.Printf("耗时：%s\n", time.Since(start))
-		return err
-	}
+	// interceptor := func (ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	// 	start := time.Now()
+		
+	// 	md := metadata.New(map[string]string{
+	// 		"APPID": "2018061036",
+	// 		"APPKEY": "Ycu@061036",
+	// 	})
+	// 	ctx = metadata.NewOutgoingContext(context.Background(), md)
+	// 	err := invoker(ctx, method, req, reply, cc, opts...)
+	// 	fmt.Printf("耗时：%s\n", time.Since(start))
+	// 	return err
+	// }
 
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithInsecure())
-	opts = append(opts, grpc.WithUnaryInterceptor(interceptor)) 
+	// opts = append(opts, grpc.WithUnaryInterceptor(interceptor)) 
+	opts = append(opts, grpc.WithPerRPCCredentials(customCredential{})) 
 	cc, err := grpc.Dial(":80", opts...)
 	if err != nil {
 		panic(fmt.Sprintf("failed to dial: %v", err))
